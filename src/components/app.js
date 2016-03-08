8f48ac703aba261a78a76e9712bfc8b9cb611c31
Jose Vildosola <ext_jovildosola@Falabella.cl>
2016-03-08 19:20:49 -0300

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
 
+// Containers and Components
+import SearchBar from '../containers/search_bar';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
